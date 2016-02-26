9f395823d3312dab4348d932e96bc402f93db179
Joseph Salisbury <salisbury.joseph@gmail.com>
2016-02-26 16:44:20 +0000

    Add webserver

--- main.go
+++ main.go
@@ -1,28 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"strings"
+	"time"
 
 	"github.com/giantswarm/pingdom-irc-healthcheck/irc"
 )
 
+const (
+	PingdomTemplate = `<pingdom_http_custom_check>
+	<status>%v</status>
+	<response_time>%v</response_time>
+</pingdom_http_custom_check>`
+)
+
+var (
+	IRCAddress = "irc.freenode.org:6667"
+	Channel    = "#giantswarm"
+	Nickname   = "bridgesupport"
+	Username   = "bridgesupport"
+	Port       = "8000"
+	NameToFind = "slackbridge"
+)
+
 func main() {
-	nameToFind := "slackbridge"
+	log.Println("Starting server on port:", Port)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+Port, nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	log.Println("Handling request")
 
 	config := irc.Config{
-		Address:  "irc.freenode.org:6667",
-		Channel:  "#giantswarm",
-		Nickname: "bridgesupport",
-		Username: "bridgesupport",
+		Address:  IRCAddress,
+		Channel:  Channel,
+		Nickname: Nickname,
+		Username: Username,
 	}
 
 	client, err := irc.NewClient(config)
 	if err != nil {
 		log.Println(err)
-		os.Exit(1)
-
+		http.Error(w, err.Error(), 500)
 	}
 
 	namesResponseChan := make(chan string)
@@ -32,14 +57,17 @@ func main() {
 	names := strings.Split(namesResponse, " ")
 	found := false
 	for _, name := range names {
-		if name == nameToFind {
+		if name == NameToFind {
 			found = true
 		}
 	}
 
+	status := "NOT OK"
 	if found {
-		log.Println("Found user:", nameToFind)
-	} else {
-		log.Println("Did not find user:", nameToFind)
+		status = "OK"
 	}
+
+	elapsedSeconds := time.Since(start).Seconds()
+
+	fmt.Fprintf(w, PingdomTemplate, status, elapsedSeconds)
 }
